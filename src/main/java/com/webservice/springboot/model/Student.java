c2ec36b2d14b024339257f9f0dfefdf42f376f39
ali <alisadeghi9333@gmail.com>
2023-05-15 01:42:03 +0430

    project commpleted

---
+++ src/main/java/com/webservice/springboot/model/Student.java
@@ -0,0 +1,10 @@
+package com.webservice.springboot.model;
+
+import java.util.List;
+
+public record Student(String id,
+                      String name,
+                      String description,
+                      List<Course> courses) {
+
+}
